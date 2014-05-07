8e56d65b735b9d246bfc25a922d1156aa119f765
Alex-ray <alex.james.ray@gmail.com>
2014-05-07 12:45:25 -0700

    seperated deferred module out into seperate module

--- concierge.js
+++ concierge.js
@@ -2,74 +2,9 @@
   "use strict" ;
 
   // Shelve
-  // Async function execution for client side and server side
-  var next;
-
-  if (typeof process === 'undefined' || typeof process.nextTick === 'undefined' ) {
-    next = function( fn ) { setTimeout( fn, 0 ) ; } ;
-  }
-
-  else {
-    next = process.nextTick  ;
-  }
-
-  function Shelve ( ) {
-    this.context ;
-    this.deferred  = [ ] ;
-
-    return this ;
-  }
-
-  /**
-   * add `fn` to deferred queue if shelve has not been triggered otherwise trigger it
-   * @param {Function} fn
-   * @return instance
-   * @api public
-   */
-
-  Shelve.prototype.defer = function defer ( fn ) {
-
-    // Check if deferreds have been triggered
-    if ( this.context !== undefined ) {
-      next( fn.bind( this.context ) ) ;
-    }
-
-    else {
-      this.deferred.push( fn ) ;
-    }
-
-    return this ;
-  }
-
-  /**
-   * trigger all deferred callbacks and bind them to `context` otherwise bind to instance context
-   * @param {Object} context
-   * @return instance
-   * @api public
-   */
-
-  Shelve.prototype.trigger = function trigger ( context ) {
-    var self = this ;
-    self.context = context === undefined ? self : context ;
-    var context = self.context ;
-
-    next( emit ) ;
-
-    return self ;
-
-    function emit ( ) {
-      for ( var i = 0; i < self.deferred.length; i++ ) {
-        var fn = self.deferred[ i ] ;
-        fn.call( self.context ) ;
-      }
-
-      self.deferred = [ ] ;
-    }
-
-  }
+  var Shelve = typeof global.Shelve === 'undefined' ? require( 'shelve' ).Shelve : global.Shelve ;
 
   // Concierge
-
   var concierge = { } ;
 
   concierge._callbacks   = { } ;
